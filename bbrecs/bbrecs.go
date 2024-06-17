c88ff20daf24238dffc3b25a209181524be3d647
Dylan Connolly <dylanconnolly145@gmail.com>
2024-06-17 15:36:53 -0600

    narrow models

--- bbrecs/bbrecs.go
+++ bbrecs/bbrecs.go
@@ -1,7 +1,6 @@
 package bbrecs
 
 import (
-	"fmt"
 	"image/jpeg"
 	"time"
 
@@ -16,59 +15,31 @@ type Group struct {
 
 type User struct {
 	ID          uuid.UUID `json:"id"`
-	DisplayName string    `json:"displayName"`
 	FirstName   string    `json:"firstName"`
 	LastName    string    `json:"lastName"`
 	PhoneNumber string    `jsone:"phoneNumber"`
 }
-
-type GroupUser struct {
-	ID      uuid.UUID `json:"id"`
-	UserID  uint
-	GroupID uint
-}
-
-type Rec struct {
-	Title string `json:"title"`
+type BaseRec struct {
+	ID    uuid.UUID `json:"id"`
+	Title string    `json:"title"`
 	// Tags enum #allow user creation?
-	AuthorComment Comment `json:"authorComment"`
-	AuthorPhotos  []Photo `json:"authorPhotos"`
-	UserPhotos    []Photo `json:"userPhotos"`
+	Author      User    `json:"author"`
+	Description string  `json:"description"`
+	UserPhotos  []Photo `json:"userPhotos"`
 	// AuthorPhotos #grab image from google api
 }
 
 type Photo struct {
-	Data []byte `json:"data"`
+	ID   uuid.UUID `json:"id"`
+	Data []byte    `json:"data"`
 	// DefaultQuality = 75
 	Options jpeg.Options
 }
 
 type Comment struct {
+	ID        uuid.UUID `json:"id"`
 	User      User      `json:"user"`
-	Comment   string    `json:"string"`
+	Content   string    `json:"content"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
-}
-
-func NewUser(firstName string, lastName string, phoneNumber string) User {
-	// can validate inputs (fname, lname, pnumber) here before sending to DB layer
-	user := User{
-		ID:          uuid.New(),
-		DisplayName: fmt.Sprintf("%s %s", firstName, lastName),
-		FirstName:   firstName,
-		LastName:    lastName,
-		PhoneNumber: phoneNumber,
-	}
-
-	return user
-}
-
-func NewGroup(name string) Group {
-	group := Group{
-		ID:         uuid.New(),
-		Name:       name,
-		InviteCode: "testCode",
-	}
-
-	return group
 }
