f15c05b5477ac4447d28922fcae17772f197f6a6
Dylan Connolly <dylanconnolly145@gmail.com>
2024-06-19 13:01:49 -0600

    add test case to user_test

--- postgres/user_test.go
+++ postgres/user_test.go
@@ -22,9 +22,23 @@ func TestCreateUser(t *testing.T) {
 	createdUser, err := us.CreateUser(context.Background(), user)
 
 	if err != nil {
-		t.Errorf("err is not nil, %v", err)
+		t.Errorf("err is not nil: %v", err)
 	}
 
 	fmt.Printf("new user: %+v", createdUser)
 
+}
+
+func TestGetUsers(t *testing.T) {
+	us := NewUserService(CreatePostgresConnPool("postgres://localhost:5432/bbrecs?sslmode=disable"))
+
+	users, err := us.GetUsers(context.Background())
+
+	if err != nil {
+		t.Errorf("err is not nil: %v", err)
+	}
+
+	if len(users) == 0 {
+		t.Errorf("length is 0, expected to have at least 1 entry")
+	}
 }
